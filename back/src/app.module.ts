de02e39ef552a2304108172ef28ebf128234a00e
Vinícius Araújo Messias <vinicius.messias4321@gmail.com>
2025-07-23 22:41:18 -0300

    feat: add db config and entity files

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host : process.env.PG_HOST,
+      port : parseInt(process.env.PG_PORT || '5432',10),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [],
+      synchronize: true
+
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
