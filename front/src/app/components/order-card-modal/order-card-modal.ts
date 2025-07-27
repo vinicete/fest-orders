bdd5b8c4e3736da3e04069c2ad81a62328079c1c
Vinícius Araújo Messias <vinicius.messias4321@gmail.com>
2025-07-27 01:15:30 -0300

    feat: add order modal

--- front/src/app/components/order-card-modal/order-card-modal.ts
+++ front/src/app/components/order-card-modal/order-card-modal.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'order-card-modal',
-  imports: [],
+  imports: [CurrencyPipe,DatePipe],
+  standalone: true,
   templateUrl: './order-card-modal.html',
   styleUrl: './order-card-modal.scss'
 })
 export class OrderCardModal {
+  activeModal = inject(NgbActiveModal)
+
   @Input()
   order: any
 }
--- front/src/app/components/order-card/order-card.ts
+++ front/src/app/components/order-card/order-card.ts
@@ -1,5 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { OrderCardModal } from '../order-card-modal/order-card-modal';
 
 @Component({
   selector: 'order-card',
@@ -11,4 +13,15 @@ export class OrderCard {
 
   @Input()
   orderData: any
+
+  private modalService = inject(NgbModal);
+
+  // 4. Crie um método para abrir o modal
+  openModal() {
+    // Abre o OrderModalComponent
+    const modalRef = this.modalService.open(OrderCardModal, { size: 'lg' });
+    
+    // Passa os dados do pedido atual para a propriedade 'order' do modal
+    modalRef.componentInstance.order = this.orderData;
+  }
 }
